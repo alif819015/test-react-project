ed3ddaa0ca849d1ebaa24e18557578c1c260662d
Mahmud Hasan <mahmudhassan11752@gmail.com>
2024-01-12 18:49:01 +0600

    used AbortController for unnecessary fetching

--- src/pages/UseEffectExamople.tsx
+++ src/pages/UseEffectExamople.tsx
@@ -2,28 +2,66 @@ import { useEffect, useState } from "react";
 
 const UseEffectExamople = () => {
   const [hedden, setHedden] = useState(false);
-  
+  const [user, setUser] = useState({ name: "", email: "" });
+  console.log(user)
+  useEffect(() => {
+    console.log('Render')
+  }, [user.name, user.email]);
   return (
     <div>
-      <button onClick={()=> setHedden((event)=>!event)}>{hedden? "show" : "hedden"}</button>
-      
-      {!hedden && <Counter /> }
+      <input
+        onBlur={(e) => setUser({ ...user, name: e.target.value })}
+        type="text"
+        name="name"
+        id="name"
+        className="border border-red-500"
+      />
+      <input
+        onBlur={(e) => setUser({ ...user, email: e.target.value })}
+        type="text"
+        name="email"
+        id="email"
+        className="border border-red-500"
+      />
+
+      {/* <button onClick={() => setHedden((event) => !event)}>
+        {hedden ? "show" : "hedden"}
+      </button>
+
+      {!hedden && <Todo />} */}
     </div>
   );
 };
 
-const Counter =()=>{
-    const [counter, setCounter] = useState(0);
+// const Counter =()=>{
+//     const [counter, setCounter] = useState(0);
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       console.log('counter')
+//       setCounter((prev) => prev + 1);
+//     }, 1000);
+//     return () => {
+//       clearInterval(intervalId);
+//     };
+//   }, []);
+//   return <h1 className="border-2 border-cyan-700 p-5">{counter}</h1>
+// }
+
+const Todo = () => {
+  const controler = new AbortController();
+  const signal = controler.signal;
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      console.log('counter')
-      setCounter((prev) => prev + 1);
-    }, 1000);
+    fetch("https://jsonplaceholder.typicode.com/todos/1", { signal })
+      .then((res) => res.json())
+      .then((data) => alert(data.title));
+
     return () => {
-      clearInterval(intervalId);
+      controler.abort();
     };
   }, []);
-  return <h1 className="border-2 border-cyan-700 p-5">{counter}</h1>
-}
+
+  console.log("data");
+  return <div>Todo</div>;
+};
 
 export default UseEffectExamople;
